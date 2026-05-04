314e57ba9922fde28651692a0a6be9a7aa0cd6be
sejalmunshii <sejalmunshi11@gmail.com>
2026-05-04 13:07:34 +0530

    Update API URL to production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://urban-diva-backend.onrender.com/api",
 });
 
 export default API;
