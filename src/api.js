5d6041a0a0403f2ea9568b17439ed8aa122f0cf4
sejalmunshii <sejalmunshi11@gmail.com>
2026-05-04 13:14:05 +0530

    Add env variable

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://urban-diva-backend.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://urban-diva-backend.onrender.com/api",
 });
+
 
 export default API;
